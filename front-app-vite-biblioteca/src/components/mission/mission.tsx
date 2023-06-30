03fb85444ac2ca95b9bac4a50fe465b5903a8163
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-06-30 18:01:10 -0300

    ideia de section misão

--- front-app-vite-biblioteca/src/components/mission/mission.tsx
+++ front-app-vite-biblioteca/src/components/mission/mission.tsx
@@ -10,7 +10,7 @@ export default function Mission() {
                 Ferramenta para promover o <br />conhecimento livre
             </h2>
             <p className={styles.descrition}>
-                Nosso site é um refúgio de conhecimento proibido, desafiando o sistema opressivo. Oferecemos acesso a livros em formato PDF que revelam verdades ocultas. Criamos uma comunidade unida contra a conspiração que aprisiona o conhecimento. Nossa biblioteca é atualizada com informações subversivas de autores corajosos. Junte-se a nós nessa revolução silenciosa, onde o conhecimento é a arma contra a opressão. Seja parte da conspiração contra o sistema, desvendando os mistérios ocultos que tentam esconder.
+                Nosso site é um refúgio para o conhecimento, desafiando as amarras impostas oferecendo acesso a uma ampla variedade de livros e segredos ocultos em formato PDF. Buscamos construir uma comunidade unida na busca pelo aprendizado e na superação das barreiras ao conhecimento. Nossa biblioteca é atualizada com informações subversivas de autores corajosos. Junte-se a nós nessa revolução silenciosa.
             </p>
         </section>
     )
