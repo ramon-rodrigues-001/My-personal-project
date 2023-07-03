1e13065909c94d14960775401c00f5f5eb1e0a33
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-03 19:32:58 -0300

    Accordion quase pronto

--- front-app-vite-biblioteca/src/components/accordion/accordion.tsx
+++ front-app-vite-biblioteca/src/components/accordion/accordion.tsx
@@ -2,12 +2,32 @@ import styles from "./accordion.module.scss";
 
 export default function Accordion() {
 
-    function active(content: string) {
-        alert('hello')
-        const accordionContent = document.querySelector(`#${content}`)
-        accordionContent.classList.toggle("._accordionIten_1k5cq_33");
-        accordionContent.classList.toggle("accordionContentOn");
-    }
+      function active(content: string) {
+        const accordionContent = document.querySelector(`#content-${content}`) as HTMLElement | null;
+
+        accordionContent?.classList.toggle(styles.accordionContentOff)
+        accordionContent?.classList.toggle(styles.accordionContentOn)
+
+        
+
+        if (accordionContent?.classList.contains(styles.accordionContentOff)) 
+        {
+            const icone = document.querySelector(`#icon${content}`) as HTMLElement | null;
+            if (icone) {
+                icone.style.transform = "rotate(360deg)"
+                icone.style.transition = ".3s"
+                icone.innerHTML = '+';
+            }
+        }
+        else {
+            const icone = document.querySelector(`#icon${content}`) as HTMLElement | null;
+            if (icone) {
+                icone.style.transform = "rotate(360deg)"
+                icone.style.transition = ".3s"
+                icone.innerHTML = '-';
+            }
+        }
+      }
 
     return (
         <section className={styles.sectionAccordion}>
@@ -20,32 +40,32 @@ export default function Accordion() {
                 
                 <div className={styles.content}>
                     <div className={styles.accordionIten}>
-                        <div className={styles.accordionHeader} onClick={() => {active("content-1")}}>
+                        <div className={styles.accordionHeader} onClick={() => {active("1")}}>
                             <span>
                                 Posso baixar os PDFs?
                             </span>
-                            <div className={styles.icon}>
+                            <div className={styles.icon} id="icon1">
                                 +
                             </div>
                         </div>
                         <div className={styles.accordionContentOff} id="content-1">
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nobis error est qui dolores dolor, ullam non dolorem quis. Minima officiis enim ea, unde odio consequatur magni fuga libero repudiandae.
+                                Sim você tem permição para baixar ipsum dolor sit amet consectetur adipisicing elit. Aut nobis error est qui dolores dolor, ullam non dolorem quis. Minima officiis enim ea, unde odio consequatur magni fuga libero repudiandae.
                             </p>
                         </div>
                     </div>
                     <div className={styles.accordionIten}>
-                        <div className={styles.accordionHeader} onClick={() => {active("content-2")}}>
+                        <div className={styles.accordionHeader} onClick={() => {active("2")}}>
                             <span>
                                 Os livros tem direitos autorais? 
                             </span>
-                            <div className={styles.icon}>
+                            <div className={styles.icon}  id="icon2">
                                 +
                             </div>
                         </div>
                         <div className={styles.accordionContentOff} id="content-2">
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nobis error est qui dolores dolor, ullam non dolorem quis. Minima officiis enim ea, unde odio consequatur magni fuga libero repudiandae.
+                                Não, todos o livros e DPFs tiverem seus direitos autorais perdidos, agora e de direito do estado ipsum dolor. Aut nobis error est qui dolores dolor, ullam non dolorem quis. Minima officiis enim ea, unde odio consequatur magni fuga libero repudiandae.
                             </p>
                         </div>
                     </div>
