9355e762925ebaf9c5875027f8456b3c9b53910e
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-05 18:44:12 -0300

    Mudando a base da configuração VITE

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-personal-project",
+  base: "https://github.com/ramon-rodrigues-001/My-personal-project",
 })
