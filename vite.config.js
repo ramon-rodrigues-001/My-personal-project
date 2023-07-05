1afab7436f5fca085ec513beca77f379dd851c1d
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-05 19:36:19 -0300

    Mudando locais dos arquivos

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist'
-  }
   // base: "https://github.com/ramon-rodrigues-001/My-personal-project",
 })
