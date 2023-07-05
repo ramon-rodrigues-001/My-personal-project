8a3b4adb971f53c61fe518fe9043f8e1b220b64f
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-05 19:59:15 -0300

    ultimas alterações do dia
    Espero que funcione

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/My-personal-project",
   plugins: [react()],
   // base: "https://github.com/ramon-rodrigues-001/My-personal-project",
 })
