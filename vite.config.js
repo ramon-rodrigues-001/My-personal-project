f777423449aefa18b9e2b7c5aa324487ed3efa95
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-07 19:22:20 -0300

    na luta

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/My-personal-project",
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: 'dev.html',
-      },
+        main: 'dev.html'
+      }
     },
-  },
-});
+    outDir: 'dist',
+    assetsDir: '', 
+    entryFileNames: 'index.html',
+    manifest: true
+  }
+})
