fbf0a8e5f9b236cbc0cff4883649569897da25b9
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-07-15 11:28:36 -0300

    incrementando front da paginação

--- src/components/vitrine/3-container-livros/containerLivros.jsx
+++ src/components/vitrine/3-container-livros/containerLivros.jsx
@@ -4,15 +4,37 @@ export default function ContainerLivros() {
     return (
         <section className={styles.sectionLivros}>
             <div className={styles.containerLivro}>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
-                <div className={styles.cardLivro}></div>
+                <div className={styles.espacoDeLivros}>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}></div>
+                </div>
+
+                <div>
+                    <ul className={styles.paginatio}>
+                        <li>
+                            <a className={styles.pageLink}>Previous</a>
+                        </li>
+                        <li>
+                            <a className={styles.pageLink}>1</a>
+                        </li>
+                        <li>
+                            <a className={styles.pageLink}>2</a>
+                        </li>
+                        <li>
+                            <a className={styles.pageLink}>3</a>
+                        </li>
+                        <li>
+                            <a className={styles.pageLink}>Next</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     )
