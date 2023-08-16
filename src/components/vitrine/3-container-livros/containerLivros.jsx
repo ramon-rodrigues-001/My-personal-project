0536c04e4d7d87861caae5d1dbb4300917bd8f0a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-08-16 19:56:26 -0300

    Testando estilos dos cards

--- src/components/vitrine/3-container-livros/containerLivros.jsx
+++ src/components/vitrine/3-container-livros/containerLivros.jsx
@@ -11,7 +11,15 @@ export default function ContainerLivros() {
                 </div>
 
                 <div className={styles.espacoDeLivros}>
-                    <div className={styles.cardLivro}></div>
+                    <div className={styles.cardLivro}>
+                        <img src="https://lirp.cdn-website.com/4deba6da/dms3rep/multi/opt/fullsize-2018-01-09-18-Peca-Grafica-230330_470658_181301872_308451081-1e8eebeb-640w.jpg" alt="" className={styles.livro1}/>
+                        <img src="/carrinho-apagar.png" alt="" className={styles.carrin}/>
+                        <h3>TECNOLOGIA O MOTO...</h3>
+                        <div>
+                            <h3 className={styles.del}><s>R$36,00</s></h3>
+                            <h3>R$10,00</h3>
+                        </div>
+                    </div>
                     <div className={styles.cardLivro}></div>
                     <div className={styles.cardLivro}></div>
                     <div className={styles.cardLivro}></div>
